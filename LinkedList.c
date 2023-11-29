1335a582fb5f74e3c7aa5e37f101cc9347574ec2
DurgeshSChauhan007 <131304057+DurgeshSChauhan007@users.noreply.github.com>
2023-11-30 01:28:55 +0530

    Add files via upload

---
+++ LinkedList.c
@@ -0,0 +1,137 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*************/
+struct node
+{
+    int info;
+    struct node *next;
+};
+/**************/
+
+struct node * GetNode()
+{
+    struct node *p;
+    p = (struct node *)malloc(sizeof(struct node));
+    return p;
+};
+
+void FreeNode(struct Node **list)
+{
+    free(*list);
+}
+void InsBeg(struct node **list, int x)
+{
+    struct node *temp;
+    temp=GetNode();
+    temp->info=x;
+    temp->next=*list;
+    *list=temp;
+}
+/***************/
+void Traverse (struct node *list)
+{
+    struct node *t;
+    t=list;
+    while(t!=NULL)
+    {
+        printf("\t  %d",t->info);
+        t=t->next;
+    }
+}
+/*************/
+void InsEnd(struct node **list, int x)
+{
+    struct node *temp,*p;
+    temp=*list;
+    while (temp->next!=NULL)
+        temp=temp->next;
+    p=GetNode();
+    p->info=x;
+    p->next=NULL;
+
+    temp->next=p;
+}
+/****************/
+void InsAft(struct node **list, int y, int x)
+{
+    struct node *temp,*p;
+    temp=*list;
+    while(temp!=NULL)
+    {
+        if (temp->info==y)
+            break;
+        else
+            temp=temp->next;
+    }
+    p=GetNode();
+    p->info=x;
+    p->next=temp->next;
+    temp->next=p;
+}
+/*************/
+int DelBeg(struct node **list)
+{
+    struct node *temp;
+    int x;
+    temp=*list;
+    (*list)=(*list)->next;
+    x=temp->info;
+    free(temp);
+    return x;
+}
+/************/
+int DelEnd(struct node **list)
+{
+    struct node *p,*q;
+    p=*list;
+    q=NULL;
+    int x;
+    while (p->next!=NULL)
+    {
+        q=p;
+        p=p->next;
+    }
+
+    q->next=NULL;
+    x=p->info;
+    free(p);
+    return x;
+}
+/************/
+void main()
+{
+    struct node *START;
+    START=NULL;
+    int x;
+    InsBeg(&START,100);
+    InsBeg(&START,200);
+    InsBeg(&START,300);
+    InsBeg(&START,400);
+    InsBeg(&START,500);
+    InsBeg(&START,600);
+    Traverse(START);
+    /***********/
+    printf("\n");
+    InsEnd(&START,800);
+    InsEnd(&START,700);
+    Traverse(START);
+    printf("\n");
+    InsAft(&START,400,350);
+    InsAft(&START,200,150);
+    Traverse(START);
+    printf("\n");
+    x=DelBeg(&START);
+    printf("Deleted Value is :=> %d",x);
+    x=DelBeg(&START);
+    printf("\nDeleted Value is :=> %d",x);
+    x=DelBeg(&START);
+    printf("\nDeleted Value is :=> %d\n",x);
+    Traverse(START);
+    x=DelEnd(&START);
+    printf("\nDeleted Value is :=> %d\n",x);
+    Traverse(START);
+    x=DelEnd(&START);
+    printf("\nDeleted Value is :=> %d\n",x);
+    Traverse(START);
+    return 0;
+}
